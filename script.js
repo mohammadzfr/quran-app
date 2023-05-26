0d217a91cc768ab04a0af51ac63a94a876a6564a
zfr <mohammadalizafar986@gmail.com>
2023-05-26 13:12:18 -0600

    displayed verses, verse number, and surah name correctly

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-// GET
-// const surahs = axios.get('https://api.quranapi.org/surahs?lang={code}').then(r => console.log(r.data));
-
-// const display = document.querySelector('.quran');
-// const words = document.createElement('p');
-// const page = axios.get('http://api.alquran.cloud/v1/page/1/quran-uthmani')
-// .then(result => {
-//     console.log(result.data.data.ayahs);
-//     result.data.data.ayahs.forEach(element =>
-//     words.textContent += element.text)
-// })
-// .catch((err) => console.log(err));
+const quran = document.querySelector('.quran');
+let surahTitle = document.createElement('h1');
+let ayahValue;
+const api = axios.get('http://api.alquran.cloud/v1/ruku/7/quran-uthmani')
+.then(r => displayVerses(r))
+.catch(err => console.log(err));
 
 
+function displayVerses(value) {
+    // console.log(value);
+    // surahTitle.textContent = value.data.data.ayahs[0].surah.englishName;
+    // console.log(value.data.data.ayahs[0].surah.englishName);
+    // quran.appendChild(surahTitle);
+    value.data.data.ayahs.forEach(ayah => {
+        if (surahTitle.textContent != ayah.surah.englishName) {
+            console.log("executed");
+            surahTitle = document.createElement('h1');
+            surahTitle.textContent = ayah.surah.englishName;
+            quran.appendChild(surahTitle);
+        }
+        ayahValue = document.createElement('p');
+        ayahValue.textContent = ayah.text + ayah.number;
+        quran.appendChild(ayahValue);
 
-// display.appendChild(words);
-
-const display = document.querySelector('.quran');
-let words
-const page = axios.get('http://api.alquran.cloud/v1/juz/30/quran-uthmani')
-.then(result => {
-    
-    result.data.data.ayahs.forEach(element => {
-        words = document.createElement('p');
-        words.classList.add('ayah');
-        words.textContent = element.text;
-        display.appendChild(words);
     })
-    
-    
-})
-.catch((err) => console.log(err));
-
-
-
-display.appendChild(words);
+}
